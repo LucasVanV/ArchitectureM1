809eb5600344ac447dc675e078388d8da6e955b3
LucasVanV <lucas.vanvooren@outlook.com>
2024-11-19 15:21:26 +0100

    tp/d avec main

---
+++ src/main/java/org/example/tp/d/enonce/CarApp.java
@@ -0,0 +1,9 @@
+package org.example.tp.d.enonce;
+
+public class CarApp {
+    public static void main(String[] args) {
+        Engine v8Biturbo = new Engine();
+        Car toyotaSUPRAAAA = new Car(v8Biturbo);
+        toyotaSUPRAAAA.start();
+    }
+}
--- src/main/java/org/example/tp/d/enonce/Engine.java
+++ src/main/java/org/example/tp/d/enonce/Engine.java
@@ -2,6 +2,6 @@ package org.example.tp.d.enonce;
 
 public class Engine {
     public void start() {
-        System.out.println("Engine started.");
+        System.out.println("VROUM VROUM");
     }
 }
